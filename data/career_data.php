48a9fe1d9ff368dd6789e3e6acc254af69b563d4
Alexandr Varugin <otz-job@mail.ru>
2018-12-20 09:41:41 +0400

    initial commit

---
+++ data/career_data.php
@@ -0,0 +1,83 @@
+<?php
+
+return [
+    'about' => 'О себе... ',
+    'education' => [
+        [
+            'title' => 'Школа',
+            'speciality' => 'Школьник',
+            'yearStart' => '1984',
+            'yearEnd' => '1994',
+        ],
+        [
+            'title' => 'Университет',
+            'speciality' => 'Студент',
+            'yearStart' => '1994',
+            'yearEnd' => '1999',
+        ],
+    ],
+    'experiences' => [
+        [
+            'title' => 'Место работы №3',
+            'post' => 'Должность №3',
+            'city' => 'Петрозаводск',
+            'company' => 'ОТЗ',
+            'about' => 'О компании',
+            'yearStart' => '2010',
+            'yearEnd' => 'Present',
+        ],
+        [
+            'title' => 'Место работы №2',
+            'post' => 'Должность №2',
+            'city' => 'Петрозаводск',
+            'company' => 'КМК',
+            'about' => 'О компании',
+            'yearStart' => '2008',
+            'yearEnd' => '2010',
+        ],
+        [
+            'title' => 'Место работы №1',
+            'post' => 'Должность №1',
+            'city' => 'Петрозаводск',
+            'company' => 'ТД АЛКОР',
+            'about' => 'О компании',
+            'yearStart' => '2001',
+            'yearEnd' => '2008',
+        ],
+    ],
+    'projects' => [
+        [
+            'title' => 'Проект #1',
+            'link' => 'http://example1.com',
+            'about' => 'О проекте',
+        ],
+        [
+            'title' => 'Проект #2',
+            'link' => 'http://example2.com',
+            'about' => 'О проекте',
+        ],
+        [
+            'title' => 'Проект #3',
+            'link' => 'http://example3.com',
+            'about' => 'О проекте',
+        ],
+    ],
+    'skills' => [
+        [
+            'title' => 'PHP',
+            'level' => '10',
+        ],
+        [
+            'title' => 'Unix',
+            'level' => '10',
+        ],
+        [
+            'title' => 'JavaScript',
+            'level' => '10',
+        ],
+        [
+            'title' => 'SQL',
+            'level' => '1',
+        ],
+    ],
+];
---
+++ data/data.php
@@ -0,0 +1,40 @@
+<?php
+return [
+    'about' => [
+        'name' => 'Александр Варыгин',
+        'photo' => '',
+        'email' => 'otz-job@mail.ru',
+        'phone' => '+7(911)400-60-63',
+        'site' => 'example.com',
+        'post' => 'engineer',
+    ],
+    'education' => [
+        [
+            'title' => 'Школа',
+            'speciality' => 'Школьник',
+            'yearStart' => '1984',
+            'yearEnd' => '1994',
+        ],
+        [
+            'title' => 'Университет',
+            'speciality' => 'Студент',
+            'yearStart' => '1994',
+            'yearEnd' => '1999',
+        ],
+    ],
+    'languages' => [
+        [
+            'title' => 'English',
+            'level' => 'Pre-intermediate'
+        ],
+        [
+            'title' => 'Russian',
+            'level' => 'Intermediate C4'
+        ],
+    ],
+    'interest' => [
+        'Кино',
+        'Машины',
+        'Программирование'
+    ],
+];
